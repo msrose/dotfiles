cc7a4959a5540935b5232a190b77856e8e5ad9c0
Michael Rose <michael_rose@outlook.com>
2017-09-11 20:29:33 -0400

    Add dotvim install

--- install.sh
+++ install.sh
@@ -2,11 +2,16 @@
 set -e
 
 $DOTFILES=$HOME/dotfiles
+$DOTVIM=$HOME/.vim
 
 git clone git@github.com:msrose/dotfiles.git $DOTFILES
 
 ln -s $DOTFILES/gitconfig $HOME/.gitconfig
 ln -s $DOTFILES/gitignore $HOME/.gitignore
 ln -s $DOTFILES/bashrc $HOME/.bashrc
+
+git clone git@github.com:msrose/dotvim.git $DOTVIM
+
+source $DOTVIM/install.sh
 
 source $DOTFILES/node/install.sh
