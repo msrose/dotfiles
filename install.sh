952f46e567c6155ab4bc73a6a284b31dddaa58dd
Michael Rose <michael_rose@outlook.com>
2024-02-16 01:39:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,17 @@ set -e
 $DOTFILES=$HOME/dotfiles
 $DOTVIM=$HOME/.vim
 
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)
+
 git clone git@github.com:msrose/dotfiles.git $DOTFILES
 
 ln -s $DOTFILES/gitconfig $HOME/.gitconfig
 ln -s $DOTFILES/gitignore $HOME/.gitignore
-ln -s $DOTFILES/bashrc $HOME/.bashrc
+ln -s $DOTFILES/zshrc $HOME/.zshrc
 
 git clone git@github.com:msrose/dotvim.git $DOTVIM
 
