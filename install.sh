26a81b8e3a31540df9c51c4fdeab7c3d8ee09a29
Michael Rose <michael_rose@outlook.com>
2024-02-16 01:46:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-set -e
+set -eo pipefail
 
 $DOTFILES=$HOME/dotfiles
 $DOTVIM=$HOME/.vim
@@ -8,6 +8,8 @@ $DOTVIM=$HOME/.vim
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew install git gh vim universal-ctags
 
 # Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)
